9b5001f9244f13c466cc4c4656a443c115f06aba
Sam Rozenberg <sam.rozen@hotmail.com>
2023-01-28 05:39:32 +0100

    changes complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,7 +26,7 @@ class EventsController < ApplicationController
         @giver = @giver_pair.participant
         @receiver = @receiver_pair.participant
 
-        EventMailer.gift_assignment_email(@event, @giver, @receiver).deliver_now
+        # EventMailer.gift_assignment_email(@event, @giver, @receiver).deliver_now
       end
 
       redirect_to event_path(@event)
